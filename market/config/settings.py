28ce1b57478961868c770f123eb8c3e01541c38d
Сергей Русанов <sergeikulyb@mail.ru>
2023-07-24 19:50:38 +0300

    Интеграция верстки шаблона сайта, poetry add django_jinja

--- market/config/settings.py
+++ market/config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "products",
     "shops",
-    'django_jinja',
+    "django_jinja",
 ]
 
 MIDDLEWARE = [
@@ -64,18 +64,17 @@ TEMPLATES = [
         "BACKEND": "django_jinja.backend.Jinja2",
         "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
-        'OPTIONS':{
+        "OPTIONS": {
             "app_dirname": "templates",
-            'environment': 'jinja2_env.environment',
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+            "environment": "jinja2_env.environment",
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
-
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR / "templates"],
--- market/jinja2_env.py
+++ market/jinja2_env.py
@@ -7,8 +7,8 @@ def environment(**options):
     env = Environment(**options)
     env.globals.update(
         {
-            'static': staticfiles_storage.url,
-            'url': reverse,
+            "static": staticfiles_storage.url,
+            "url": reverse,
         }
     )
     return env
