98b3bf2b73a2e07c966492183b6b0f6176125d72
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-07-25 10:08:05 -0500

    Прописал настройки конфигурации для отправки email.

--- market/config/settings.py
+++ market/config/settings.py
@@ -77,6 +77,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+# email configs
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = config["EMAIL_USER"]
+EMAIL_HOST_PASSWORD = config["EMAIL_PASSWORD"]
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
