2c19f0818387b95bc619f708417bf54c95047e17
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-07-27 05:24:01 -0500

    Добавил конфигурации почты. Подкорректировал url

--- market/profiles/urls.py
+++ market/profiles/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path("password-reset/", ResetPasswordView.as_view(), name="password_reset"),
     path("password-reset-sent/", ResetPasswordDoneView.as_view(), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", ResetPasswordConfirmView.as_view(), name="password_reset_confirm"),
-    path("password-reset/", ResetPasswordCompleteView.as_view(), name="password_reset_complete"),
+    path("password-reset-complete/", ResetPasswordCompleteView.as_view(), name="password_reset_complete"),
 
 ]
