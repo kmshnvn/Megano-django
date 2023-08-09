b89c4fb8b0b62b1ccfb0a1fe30c822ad4afa2ecc
Rusanov Sergey <sergeikulyb@mail.ru>
2023-08-09 21:18:32 +0300

    Изменение вкладки цены в шаблоне, правки по flake8 и black

--- market/shops/views.py
+++ market/shops/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView, DetailView
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404
 from .models import (
     Offer,
-    Shop,
 )
 from products.models import (
     Product,
-ProductDetail,
+    ProductDetail,
 )
 
 
 class ProductDetailViev(View):
     def get(self, request: HttpRequest, pk: int) -> HttpResponse:
         offer = Offer.objects.filter(product_id=pk)
-        product  = Product.objects.get(pk=pk)
+        product = Product.objects.get(pk=pk)
         product_detail = ProductDetail.objects.get(product_id=pk)
         context = {
             "offers": offer,
             "product": product,
             "product_detail": product_detail,
+            "min_price": min([off.price for off in offer])
         }
-        return render(request, 'shops/product-detail.jinja2', context=context)
+        return render(request, "shops/product-detail.jinja2", context=context)
