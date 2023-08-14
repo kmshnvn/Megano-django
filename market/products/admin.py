eb04a45b14b04a86d0e97f7168de09293ed4f695
Алан Джелиев <djalan84@mail.ru>
2023-08-14 18:39:31 +0300

    Update admin.py

--- market/products/admin.py
+++ market/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Detail, ProductDetail, Banner
+from .models import Category, Product, Detail, ProductDetail, Banner
 
 
 @admin.register(Banner)
