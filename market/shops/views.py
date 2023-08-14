1ea2d8cfc59a22ca25b970e4f74e3f44d08fed3a
@timur_karkoshkin <kivara666@gmail.com>
2023-08-14 16:24:11 +0300

    KISS method realised

--- market/shops/views.py
+++ market/shops/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render  # noqa F401
 from django.views.generic.detail import DetailView
-from .models import Shop, Offer
-from products.models import Product
-from django.http import HttpRequest, HttpResponse
+from .models import Shop
 
 
 class ShopDetailView(DetailView):
@@ -10,15 +8,3 @@ class ShopDetailView(DetailView):
 
     template_name = "shop/shop_detail.jinja2"
     model = Shop
-
-    def get(self, request: HttpRequest, pk: int) -> HttpResponse:
-        shop = Shop.objects.get(pk=pk)
-        offer = Offer.objects.filter(shop_id=shop.pk)
-        product = Product.objects.get(pk=pk)
-
-        context = {
-            "product": product,
-            "shop": shop,
-            "offer": offer
-        }
-        return render(request, "shop/shop_detail.jinja2", context=context)
