335a77fa53dd228d0794bf8d8660488400087e47
Алан Джелиев <djalan84@mail.ru>
2023-08-14 15:54:54 +0300

    Выполнения миграций

--- market/products/models.py
+++ market/products/models.py
@@ -6,7 +6,7 @@ class Banner(models.Model):
     """Баннер"""
 
     image = models.ImageField(blank=True, upload_to="banners/image")
-    link = models.URLField(max_length=200)
+    link = models.URLField(max_length=200, blank=True, null=True)
     description = models.TextField(max_length=200, blank=True, null=True)
 
 
