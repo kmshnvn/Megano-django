ec40bf5a2b66da1e0cd102ba9819637d03210808
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-08-16 01:06:17 -0500

    Изменил родительский класс в классе представления ProductDetailViev

--- market/products/views.py
+++ market/products/views.py
@@ -2,10 +2,9 @@ from comments.forms import CommentAddForm
 from comments.models import Comment
 from django.shortcuts import render
 from django.urls import reverse
-from django.views import View
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.db.models import Min
-from django.views.generic.edit import FormMixin
+from django.views.generic.edit import CreateView
 from shops.models import (
     Offer,
 )
@@ -15,18 +14,19 @@ from .models import (
 )
 
 
-class ProductDetailViev(FormMixin, View):
+class ProductDetailViev(CreateView):
     form_class = CommentAddForm
-    context = {}
+    template_name = "products/product-detail.jinja2"
 
-    def get(self, request: HttpRequest, pk: int) -> HttpResponse:
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        pk = kwargs["pk"]
         offers = Offer.objects.prefetch_related("shop").filter(product_id=pk)
         product_details = ProductDetail.objects.prefetch_related("detail", "product").get(product_id=pk)
 
         comments = Comment.get_list_comments(product_pk=pk)
         comment_count = Comment.get_number_comments(product_pk=pk)
 
-        self.context = {
+        context = {
             "offers": offers,
             "product": product_details.product,
             "product_detail": product_details,
@@ -34,23 +34,15 @@ class ProductDetailViev(FormMixin, View):
             "comments_list": comments,
             "comment_count": comment_count,
         }
-        return render(request, "products/product-detail.jinja2", context=self.context)
+        return render(request, "products/product-detail.jinja2", context=context)
 
-    def post(self, request: HttpRequest, pk: int) -> HttpResponse:
-        """Принимаем данные формы, сохраняем новый отзыв в БД."""
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.author = self.request.user
+        comment.product = Product.objects.get(pk=self.kwargs["pk"])
+        comment.save()
 
-        form = CommentAddForm(request.POST)
-        if form.is_valid():
-            product = Product.objects.get(pk=pk)
-
-            new_comment = form.save(commit=False)
-            new_comment.author = request.user
-            new_comment.product = product
-            new_comment.save()
-            return self.form_valid(form)
-
-        self.context["form"] = form
-        return render(request, "products/product-detail.jinja2", context=self.context)
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse("products:product_detail", kwargs={"pk": self.kwargs["pk"]})
