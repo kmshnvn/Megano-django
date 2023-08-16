4f2e5ce02cba0d92cfbe4989894dfa5fd24fe42e
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-08-16 14:02:18 -0500

    Убрал методы модели

--- market/comments/models.py
+++ market/comments/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import QuerySet
 from products.models import Product
 from django.utils.translation import gettext_lazy as _
 
@@ -24,30 +23,3 @@ class Comment(models.Model):
 
     def __str__(self) -> str:
         return f"Отзыв {self.author.username} о продукте {self.product.name}"
-
-    @classmethod
-    def get_number_comments(cls, product_pk: int) -> int:
-        """
-        Метод получения количества отзывов для товара.
-
-        :param product_pk: Передает pk модели Product.
-        :return count: Число отзывов.
-        """
-
-        count = cls.objects.filter(product_id=product_pk).count()
-        return count
-
-    @classmethod
-    def get_list_comments(cls, product_pk: int, offset: int = 0, limit: int = 10) -> QuerySet:
-        """
-        Метод получения список отзывов к товару.
-        Принимает pk модели Product, а также смещения и лимит возвращаемого результата.
-
-        :param product_pk: Передает pk модели Product.
-        :param offset: Передает количество записей, которые необходимо пропустить.
-        :param limit: Передает лимит записей, которые необходимо извлечь.
-        :return: QuerySet, Список отзывов о товаре.
-        """
-
-        comments = cls.objects.select_related("author", "product").filter(product_id=product_pk)[offset:limit]
-        return comments
