1dcdf60e64050594028f66c4ba080f807f276b9a
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-08-17 00:42:40 -0500

    Исправил импорт

--- market/comments/tests.py
+++ market/comments/tests.py
@@ -29,18 +29,3 @@ class CommentModelTestCase(TestCase):
         self.assertEqual(self.text, new_test_comment.text)
         self.assertTrue(Product.objects.filter(comments=new_test_comment).exists())
         self.assertTrue(User.objects.filter(comments=new_test_comment).exists())
-
-    def test_method_get_number_comments(self):
-        """Тестируем работу метода получения количества отзывов для товара."""
-
-        count = Comment.get_number_comments(product_pk=1)
-
-        self.assertEqual(count, 1)
-
-    def test_method_glist_comments(self):
-        """Тестируем работу метода получения список отзывов к товару."""
-
-        first_query = Comment.objects.filter(product=self.product)
-        second_query = Comment.get_list_comments(product_pk=1)
-
-        self.assertQuerySetEqual(first_query, second_query)
