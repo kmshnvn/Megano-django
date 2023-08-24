9461292026a197538eb34cf1f13a5efded1dc474
Rusanov Sergey <sergeikulyb@mail.ru>
2023-08-24 22:04:46 +0300

    Добавление моделей корзина и комментарий в админку

--- market/basket/admin.py
+++ market/basket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Basket
 
-# Register your models here.
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = "pk", "user", "offer", "product", "amount"
+    list_display_links = "pk", "user", "offer", "product", "amount"
