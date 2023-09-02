a28abc10e768406d91635ad100733d29a315fe32
Yaroslav Dmytrovskyi <dmiyaroslav92@gmail.com>
2023-09-02 09:48:35 -0500

    black

--- market/basket/basket.py
+++ market/basket/basket.py
@@ -7,7 +7,7 @@ from decimal import Decimal
 import random
 
 
-class BasketObject():
+class BasketObject:
     """Инициализируем работу корзины для добавления товаров"""
 
     def __init__(self, request: HttpRequest) -> None:
