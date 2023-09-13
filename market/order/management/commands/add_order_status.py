693fc1a89741090ff48350a9ec5e81069517718c
Rusanov Sergey <sergeikulyb@mail.ru>
2023-09-13 21:32:42 +0300

    Создание шаблонов и пропись команды на добавление статусов заказов

---
+++ market/order/management/commands/add_order_status.py
@@ -0,0 +1,15 @@
+﻿from order.models import OrderStatus
+from django.core.management import BaseCommand
+from django.db import transaction
+
+
+ORDER_STATUS_NAMES = ["создан", "оплачен", "доставляется", "завершен", "отменен"]
+
+class Command(BaseCommand):
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        self.stdout.write("Создаем статусы заказа...")
+        for status in ORDER_STATUS_NAMES:
+            OrderStatus.objects.create(name=status)
+        self.stdout.write("Успешно создано!")
