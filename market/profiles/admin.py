b20b4c965ff966d0f51550c121fab1d8dd526d9c
Rusanov Sergey <sergeikulyb@mail.ru>
2023-09-20 22:43:41 +0300

    Добавления pk в полях админ панели

--- market/profiles/admin.py
+++ market/profiles/admin.py
@@ -4,8 +4,8 @@ from profiles.models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = "user", "address", "phone", "balance"
-    list_display_links = "user", "address", "phone", "balance"
+    list_display = "pk", "user", "address", "phone", "balance"
+    list_display_links = "pk", "user", "address", "phone", "balance"
 
 
 # Register your models here.
