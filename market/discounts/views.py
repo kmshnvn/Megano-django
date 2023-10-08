b056f7f662ba0f5afeee66334854ae56c7673f04
Yaroslav Dmytrovskyi <gstonerus@yandex.ru>
2023-10-08 12:23:13 -0600

    Создания предоставления для отображения списка скидок.

--- market/discounts/views.py
+++ market/discounts/views.py
@@ -1,3 +1,19 @@
-# from django.shortcuts import render
+from itertools import chain
 
-# Create your views here.
+from discounts.models import ProductDiscount, CategoryDiscount, BasketDiscount
+from django.views.generic import ListView
+
+
+class DiscountsListView(ListView):
+    """Класс представления списка всех скидок на сайте."""
+
+    template_name = "discount/discounts-list.jinja2"
+    context_object_name = "discounts"
+
+    def get_queryset(self):
+        qs1 = ProductDiscount.objects.filter(active=True)
+        qs2 = CategoryDiscount.objects.filter(active=True)
+        qs3 = BasketDiscount.objects.filter(active=True)
+
+        queryset = list(chain(qs1, qs2, qs3))
+        return queryset
